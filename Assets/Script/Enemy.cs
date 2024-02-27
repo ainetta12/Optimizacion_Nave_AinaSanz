329191a789978c9290711f349857171a858e6c16
ainetta12 <ainasanzg@gmail.com>
2024-02-27 17:39:09 +0100

    enemy

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-      [SerializeField] float enemySpeed = 5;
+    
+    [SerializeField] float enemySpeed = 5;
 
     
     void Update()
     {
-        transform.position += transform.forward * 2 * Time.deltaTime;
+        transform.position += transform.forward * enemySpeed * Time.deltaTime;
     }
 
     void OnCollisionEnter(Collision colision)
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -5,7 +5,33 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    
+    public GameObject enemyPrefab; 
+    public float spawnInterval = 2f; 
+    public int maxEnemies = 5; 
+    [SerializeField] Transform spawnPosition;
+    [SerializeField] int enemyType = 0;
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0f, spawnInterval);
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject enemy = PoolManager2.Instance.GetPooledObjects(enemyType, spawnPosition.position, spawnPosition.rotation);
+            
+        if(enemy != null)
+            {   
+            enemy.SetActive(true);
+            }
+            else 
+            {
+            Debug.LogError("Pool demasiado pequeño");
+            }       
+ 
+    }
+
 
 
 }
